b252eade4bc5c1f0a8b832fa738a58949cd2abab
SubhajitMishra04 <96262810+SubhajitMishra04@users.noreply.github.com>
2022-02-15 09:32:07 +0530

    .

--- try.js
+++ try.js
@@ -1,44 +1,45 @@
 var app = angular.module('myApp', [], ['ui.bootstrap']);
 var app = angular.module('myApp', ['ngRoute']);
-app.config(function($routeProvider) {
+app.config(function ($routeProvider) {
     $routeProvider
 
         .when('/', {
-        templateUrl: 'about.html',
-        controller: 'FirstController'
-    })
+            templateUrl: 'about.html',
+            controller: 'FirstController'
+        })
 
-    .when('/train', {
-        templateUrl: 'train.html',
-        controller: 'SecondController'
-    })
+        .when('/train', {
+            templateUrl: 'train.html',
+            controller: 'SecondController'
+        })
 
-    .when('/trains', {
-        templateUrl: 'trains.html',
-        controller: 'ThirdController'
-    })
+        .when('/trains', {
+            templateUrl: 'trains.html',
+            controller: 'ThirdController'
+        })
 
-    .otherwise({
-        redirectTo: '/'
-    });
+        .otherwise({
+            redirectTo: '/'
+        });
 });
 
 
-app.controller('FirstController', function($scope) {
-    $scope.message = 'The Railway Administration reserves seats, berths, compartments, or carriage in accordance with the rules and conditions published in the Coaching Tariff. A passenger seeking reservation of berth or seats should purchase tickets from the Railway Reservation Offices/Authorised Travel Agency only.Advance reservations are made generally up to 120 days in advance for all classes and all trains. The period of advance reservation (ARP) is exclusive of the day of departure of the train.At intermediate stations where the train arrives on the following day, such reservations can be done more than 120 days in advance of date of journey from the intermediate station. ARP is in relation to the date of journey from train originating station. In case of some Inter-city day trains, the ARP is less.An individual can book only up to six passengers on one requisition form provided all passengers are for the same destination and for the same train.w.e.f. 01-12-2012,any one of the passenger booked on a PNR for undertaking journey in any class will have to produce any one of the 10 prescribed proofs of identity in original) during the journey failing which all  the passengers booked on that ticket will be treated as traveling  without ticket and charged accordingly. (ref. Railway Board letter no.2011/TG-I/20/P/ID dated 01-11-2012) 1.Voter Photo identity card issued by Election Commision of India. 2.Passport. ';
+app.controller('FirstController', function ($scope) {
+    $scope.message = 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officiis quas illo temporibus! Dolor, deserunt dolorum dolores impedit rerum iusto eaque id corrupti pariatur, minus, accusantium aperiam doloribus porro aliquam non! Ad, eius esse unde sit consectetur in amet rem nihil quidem obcaecati aperiam quasi explicabo molestias magni tempora aut laboriosam possimus. Eaque est libero quidem ducimus. Voluptas aliquid nam est nihil veniam ut cupiditate sequi, reprehenderit, similique adipisci tempore magnam ea. Repellat distinctio eos doloremque debitis excepturi quisquam quod molestias, commodi praesentium aliquam inventore vel laboriosam odio culpa error placeat vero unde consequuntur maxime blanditiis. Pariatur deleniti suscipit iste facilis! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officiis quas illo temporibus! Dolor, deserunt dolorum dolores impedit rerum iusto eaque id corrupti pariatur, minus, accusantium aperiam doloribus porro aliquam non! Ad, eius esse unde sit consectetur in amet rem nihil quidem obcaecati aperiam quasi explicabo molestias magni tempora aut laboriosam possimus. Eaque est libero quidem ducimus. Voluptas aliquid nam est nihil veniam ut cupiditate sequi, reprehenderit, similique adipisci tempore magnam ea. Repellat distinctio eos doloremque debitis excepturi quisquam quod molestias, commodi praesentium aliquam inventore vel laboriosam odio culpa error placeat vero unde consequuntur maxime blanditiis. Pariatur deleniti suscipit iste facilis!';
 });
 
-app.controller('SecondController', function($scope, $http) {
+app.controller('SecondController', function ($scope, $http) {
     $http.get('https://subhajitmishra04.github.io/hospital-management/record.json')
-        .success(function(response) {
+        .success(function (response) {
             $scope.names = response.records;
         });
 });
 
-app.controller('ThirdController', function($scope, $http) {
+app.controller('ThirdController', function ($scope, $http) {
     $http.get('https://subhajitmishra04.github.io/hospital-management/record.json')
-        .success(function(response) {
+        .success(function (response) {
             $scope.names = response.records;
             $scope.rowlimit = 6;
         });
 });
+
